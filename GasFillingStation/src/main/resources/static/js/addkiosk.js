2109a0b002c4019f76a7ac30983ce659ff96142e
YT00522 <Tom.Zhang>
2015-12-12 17:57:47 +0800

    meme

--- GasFillingStation/src/main/resources/static/js/addkiosk.js
+++
@@ -1,9 +0,0 @@
-app.controller('addkiosk', function($scope) {
-  $scope.addKiosk = 'result';
-  $scope.submitAddKiosk = function() {
-    $scope.addKiosk = 'result';
-  }
-  $scope.addAnother = function() {
-    $scope.addKiosk = 'setting';
-  }
-})
--- GasFillingStation/src/main/resources/static/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-window.localStorage.setItem('more',"MORE");
-window.localStorage.setItem('navigation','HOME');
-
-app.controller("user", function($scope,$http){
-    $http.get("/api/user/list").success(function(data){
-        console.log(data);
-    })
-})
